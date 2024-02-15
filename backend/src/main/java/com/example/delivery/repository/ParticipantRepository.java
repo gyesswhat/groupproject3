73c2e7cd90f2f290939557fc1d67571e9ed7fda9
gyesswhat <h2weirdo@icloud.com>
2024-02-15 21:09:54 +0900

    feat: post 상태 변경 scheduler 추가

--- backend/src/main/java/com/example/delivery/repository/ParticipantRepository.java
+++ backend/src/main/java/com/example/delivery/repository/ParticipantRepository.java
@@ -1,6 +1,7 @@
 package com.example.delivery.repository;
 
 import com.example.delivery.entity.Participant;
+import com.example.delivery.entity.Post;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -18,4 +19,8 @@ public interface ParticipantRepository extends CrudRepository<Participant, Long>
             "FROM Participant p " +
             "WHERE p.post = :postId")
     List<Participant> findByPostId(Long postId);
+
+    int getPartNum(Post post);
+
+    int getDepositCheckedNum(Post post);
 }
